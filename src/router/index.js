b97968d1e9ac4e09c260376d7af089ba7a706b9d
Damien Martinez <damienca.martinez@gmail.com>
2021-03-15 11:59:14 +0100

    Ajoout partiel de Chatastrophe

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Chatastrophe from "@/components/Chatastrophe";
 
 Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
 
 const routes = [
   {
@@ -17,6 +21,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/chatastrophe',
+    name: 'Chatastrophe',
+    component: Chatastrophe
   }
 ]
 
